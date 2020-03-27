78e9e3e0bba9621b910124423a4875ebc90329c6
Widiyantoko <widi@fairtech.com.sg>
2020-03-27 17:31:26 +0800

    initial commit

---
+++ src/main/java/gHost/promocarouselapi/PromoCarouselApiApplication.java
@@ -0,0 +1,13 @@
+package gHost.promocarouselapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCarouselApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoCarouselApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gHost/promocarouselapi/PromoCarouselApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gHost.promocarouselapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoCarouselApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
