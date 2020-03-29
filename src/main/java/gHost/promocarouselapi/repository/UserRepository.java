97b87651276b46427824332a7ba2ec4c4cfc4df5
widi <widiyantoko@ymail.com>
2020-03-29 17:39:50 +0800

    add exception and implement jpa repository

---
+++ src/main/java/gHost/promocarouselapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package gHost.promocarouselapi.repository;
+
+import gHost.promocarouselapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
+
