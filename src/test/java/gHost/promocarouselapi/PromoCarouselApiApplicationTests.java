944516fe2ef1c61e8b4e451db205657552b990b1
widi <widiyantoko@ymail.com>
2020-03-30 01:11:55 +0800

    add unit testing

--- src/test/java/gHost/promocarouselapi/PromoCarouselApiApplicationTests.java
+++ src/test/java/gHost/promocarouselapi/PromoCarouselApiApplicationTests.java
@@ -1,13 +1,45 @@
 package gHost.promocarouselapi;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-class PromoCarouselApiApplicationTests {
+@AutoConfigureMockMvc
+@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PromoCarouselApiApplication.class)
+public class PromoCarouselApiApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetUserModules() throws Exception {
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get( "/api/v1/user/1");
+		this.mockMvc.perform(requestBuilder)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.module.length()").value(5));
+	}
+
+	@Test
+	public void testGetModuleNullUser() throws Exception {
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get( "/api/v1/user/4");
+		this.mockMvc.perform(requestBuilder)
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
 	}
 
 }
